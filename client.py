9858aaeac1d2d5f692f6266874009223629c3e25
IlincaAndrei <153225626+IlincaAndrei@users.noreply.github.com>
2025-07-21 19:10:38 +0300

    Add files via upload
    bug fix la linia 87 din client.py si adaptarea MOTOR_PINS in server.py

--- client.py
+++ client.py
@@ -8,9 +8,12 @@
 # laptopul o să fie clientul (se conectează la server), iar placa o să fie serverul de TCP (așteaptă conexiune de la client)
 
 
-import common
+
+from common import *
 import socket
 import sys
+from time import sleep
+import pickle
 
 
 def init_connection(host_ip, port):
@@ -24,10 +27,12 @@ def init_connection(host_ip, port):
 
     return sock
 
+def send_command(sock, data):
+    sock.send(data)
 
+# GPIO init
 
-
-
+# TCP socket init
 
 num_args = len(sys.argv)
 if(num_args != 3):
@@ -44,12 +49,56 @@ except:
     print("Port invalid!")
     sys.exit(0)
 
-
-
 print("test")
 HOST_PORT = int(HOST_PORT)
 
 
-init_connection(HOST_IP, HOST_PORT)
+sock = init_connection(HOST_IP, HOST_PORT)
 
 
+# aici ar veni partea de generare de traiectorie
+
+while True:
+    # aici e placeholder, momentan nu fac nimica cu data
+    sensor_data = sock.recv(SENSOR_BUF_SIZE)
+    print(sensor_data)
+
+    
+    num_commands = input("enter number of commands")
+    num_commands= int(num_commands)
+
+    dt = float(input("enter command interval"))
+
+    data = CommandDataArray(dt, num_commands, [])
+    for i in range(num_commands):
+        
+        command_str= input("insert command at moment " + str(i))
+
+        parts = command_str.strip().split()
+        if len(parts) != 4:
+            print("Invalid command format. Please enter: [float] [int] [float] [int]")
+            continue
+        try:
+            float1 = float(parts[0])
+            int1 = int(parts[1])
+            float2 = float(parts[2])
+            int2 = int(parts[3])
+            
+            command = CommandData(leftMotorTuration=float1, leftMotorOrientation=int1, rightMotorTuration=float2, rightMotorOrientation=int2)
+            data.commandArray.append(command)
+
+        except ValueError:
+            print("Invalid values. Please enter: [float] [int] [float] [int]")
+            i-=1
+            continue
+    
+    
+    encoded_data = pickle.dumps(data)
+    with open('filename.pickle', 'wb') as handle:
+        pickle.dump(encoded_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    send_command(sock, encoded_data)        
+
+    # clientul (laptopul), așteaptă date de la senzori, face calculele, trimite la server (placă) comanda, rinse and repeat
+
+
+#print("success")
--- server.py
+++ server.py
@@ -1,27 +1,83 @@
-#Codul pentru placă
-
-import common
+from common import *
 import socket
-import sys
+from time import sleep
 import netifaces as ni
+from gpiozero import PWMOutputDevice, OutputDevice
+import pickle
 
 SERVER_PORT = 5010
-WIFI_INTERFACE = 'wlan0' # numele interfeței de rețea pe care se conectează placa, necesar pt a afla adresa IP asignată plăcii
+WIFI_INTERFACE = 'wlan0'
+
+# Pini: 2 PWM + 4 direcție
+MOTOR_PINS = {
+    'leftPwm': 13,        # ENA
+    'rightPwm': 16,       # ENB
+    'leftForward': 17,    # IN1
+    'leftBackward': 27,   # IN2
+    'rightForward': 23,   # IN3
+    'rightBackward': 24   # IN4
+}
+
 
 def init_connection(host_ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((host_ip, port))
     sock.listen(1)
-
     conn, addr = sock.accept()
-
     return sock, conn, addr
 
 
+# GPIO init
+leftPwm = PWMOutputDevice(MOTOR_PINS["leftPwm"])
+rightPwm = PWMOutputDevice(MOTOR_PINS["rightPwm"])
 
+leftForward = OutputDevice(MOTOR_PINS["leftForward"])
+leftBackward = OutputDevice(MOTOR_PINS["leftBackward"])
+rightForward = OutputDevice(MOTOR_PINS["rightForward"])
+rightBackward = OutputDevice(MOTOR_PINS["rightBackward"])
 
-
+# Networking init
 ip = ni.ifaddresses(WIFI_INTERFACE)[ni.AF_INET][0]['addr']
 print(ip)
-init_connection(ip, SERVER_PORT)
+
+sock, conn, addr = init_connection(ip, SERVER_PORT)
+print("success")
+
+def set_motor_direction(forward_pin, backward_pin, orientation):
+    if orientation == 1:
+        forward_pin.on()
+        backward_pin.off()
+    elif orientation == -1:
+        forward_pin.off()
+        backward_pin.on()
+    else:  # stop / frână
+        forward_pin.off()
+        backward_pin.off()
+
+
+try:
+    while True:
+        sensor_data = "test".encode()
+        conn.send(sensor_data)
+
+        encoded_input_data_array = conn.recv(COMMAND_BUF_SIZE)
+        input_data_array = pickle.loads(encoded_input_data_array)
+
+        for input_data in input_data_array.commandArray:
+            # Setează direcția
+            set_motor_direction(leftForward, leftBackward, input_data.leftMotorOrientation)
+            set_motor_direction(rightForward, rightBackward, input_data.rightMotorOrientation)
+
+            # Setează viteza
+            leftPwm.value = input_data.leftMotorTuration
+            rightPwm.value = input_data.rightMotorTuration
+
+            sleep(input_data_array.dt)
+
+except Exception as exception:
+    print("exception " + type(exception).__name__ + " raised, shutting down")
+    conn.close()
+
+conn.close()
+
 
